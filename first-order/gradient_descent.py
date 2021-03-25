0be36378684de8dcb2273c0682f8d5fa817f62eb
komour <komaroff404@gmail.com>
2021-03-25 22:51:31 +0300

    Add 2nd and 3rd tasks

--- first-order/gradient_descent.py
+++ first-order/gradient_descent.py
@@ -1,4 +1,7 @@
-def gradient_descent(f, f_grad, start_arg, method, max_iterations=1000, eps=1e-5):
+import numpy as np
+
+
+def gradient_descent(f, f_grad, start_arg, method, max_iterations=100, eps=1e-5):
     cur_arg = start_arg
     trace = [cur_arg]
     for _ in range(max_iterations):
@@ -9,6 +12,28 @@ def gradient_descent(f, f_grad, start_arg, method, max_iterations=1000, eps=1e-5
         next_arg = cur_arg - cur_step * cur_grad
         trace.append(next_arg)
 
-        if numpy.linalg.norm(cur_grad) < eps:
+        if np.linalg.norm(cur_grad) < eps:
             return trace  # answer is trace[-1]
         cur_arg = next_arg
+
+
+def make_level_lines_plot(f, f_grad, start, method, x_min, x_max, y_min, y_max, x_step=0.1, y_step=0.1, levels=None):
+    trace = gradient_descent(f, f_grad, start, method, eps=1e-8)
+    print(f'result: {trace[-1]}', f'iterations: {len(trace)}')
+    if levels is None:
+        levels = [f(args) for args in trace]
+        list.sort(levels)
+    x_s = np.arange(x_min, x_max, x_step)
+    y_s = np.arange(y_min, y_max, y_step)
+    z_s = np.array([[f(np.array([x, y])) for x in x_s] for y in y_s])
+
+    plt.figure()
+    cs = plt.contour(x_s, y_s, z_s, levels=levels)
+    plt.clabel(cs)
+    for i in range(len(trace) - 1):
+        cur_point = trace[i]
+        next_point = trace[i + 1]
+        plt.scatter([cur_point[0]], [cur_point[1]])
+        plt.plot([cur_point[0], next_point[0]], [cur_point[1], next_point[1]])
+    plt.grid()
+    plt.show()
